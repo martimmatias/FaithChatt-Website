daf85c04062dd146ff75eaf0ea61964b6307e660
Martim Matias <martimviseu@live.com.pt>
2023-08-03 18:55:53 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const ejs = require("ejs");
+const axios = require("axios");
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+require("dotenv").config();
+
+app.set("view engine", ejs);
+app.use("/public", express.static("public"));
+
+app.get("/", async (req, res) => {
+  let data = {
+    approximate_member_count: 1400, // Default value
+  };
+  // Get the member count from the Discord API
+  try {
+    const response = await axios.get(
+      "https://discord.com/api/v9/invites/faithchatt?with_counts=true"
+    );
+    data = response.data;
+  } catch (error) {
+    console.error(error);
+  }
+
+  res.render("index.ejs", {
+    member_count: (
+      data.approximate_member_count -
+      (data.approximate_member_count % 100)
+    ).toLocaleString("en-US"),
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
